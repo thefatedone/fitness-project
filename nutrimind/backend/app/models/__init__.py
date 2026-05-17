5ad42025aff686d24a9cece2493ff63f42f0e7a1
thefatedone <alexpepo2006@gmail.com>
2026-05-17 19:48:44 +0400

    cleanup: remove duplicate nutrimind/backend/, keep only root backend/

--- nutrimind/backend/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.models.user import User
-from app.models.food_log import FoodLog, WeightLog, WaterLog
-from app.models.chat import ChatMessage
-
-__all__ = ["User", "FoodLog", "WeightLog", "WaterLog", "ChatMessage"]
